4b93ad00da9994cd3889188cbe45e71ed358dff5
Charlie Rogers <chubahay@gmail.com>
2024-03-03 00:53:07 +0000

    removing bin / adding to gitignore

--- bin/data/dynatrace-env.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dynatraceUrls = void 0;
-exports.dynatraceUrls = {
-    dev: "http://localhost:3000",
-    test: "http://localhost:3001",
-    preprod: "http://localhost:3002",
-    prod: "http://localhost:3003"
-};
